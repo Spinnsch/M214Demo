021163334503f8dcea7ba73433afd4fb736b2ac4
AlexanderStengele <134547719+Spinnsch@users.noreply.github.com>
2024-01-14 18:48:34 +0100

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Hello World Ausgabe
+    cout << "Hello, World!" << endl;
+    //Neue zusätzliche Ausgabe des Schülers zwischen den "".
+    cout << "" << endl;
+    return 0;
+}
